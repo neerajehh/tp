d5370b3d7426542ef37ed692fcf1a6e4530d44d7
VinayVR26 <133363378+VinayVR26@users.noreply.github.com>
2026-03-26 16:06:46 +0800

    Create ModulesLoader.java class to load modules information from the txt files

---
+++ src/main/java/seedu/classmate/ModulesLoader.java
@@ -0,0 +1,11 @@
+package seedu.classmate;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ModulesLoader {
+    private static final String CORE_MODULES_PATH = "./data/core-modules.txt";
+    private static final String SPECIALISATION_MODULES_PATH = "./data/specialisation-modules.txt";
+}
